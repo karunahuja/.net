df4132c8c12be766390339bce3e74a23bd9876df
karun <karunahuja1998@gmail.com>
2020-08-01 14:24:58 +0530

    Starting with empty controller and adding some actions

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public string EmployeeProfile(int id)
+        {
+            string profile = string.Empty;
+            if (id==1) 
+            { 
+                return "Profile 1"; 
+            } if (id==2) 
+            { 
+                return "Profile 2"; 
+            } if (id==3) 
+            { 
+                return "Profile 3"; 
+            }
+            else
+            {
+                return "No record found";
+            }
+
+           
+        }
+        public string Address(int id, int? code = null)
+        {
+            return "id=" + id + "dept=" + code;
+        }
+    }
+}
